07782c38dd2a547efe02413fc5640eb06274f79a
Joseph Francia <josephfrancia@Josephs-MacBook-Pro.local>
2018-02-21 21:29:10 -0800

    new tests

--- R/calculate_returns.R
+++ R/calculate_returns.R
@@ -4,7 +4,9 @@ computeStockMarketReturns <- function(initialInvestment,
                                       capitalGains = .15,
                                       managementFees = .004) {
 
-  return((initialInvestment * (1 + marketReturn - managementFees)^timeHorizon) * (1 - capitalGains))
+  return((initialInvestment * (1 + marketReturn - managementFees)^timeHorizon) - 
+        (((initialInvestment * (1 + marketReturn - managementFees)^timeHorizon) - initialInvestment) * 
+          capitalGains))
 }
 
 
@@ -38,29 +40,50 @@ computeHousingReturns <- function(homeValue,
   return(accumulatedValueofHome + accumulatedValueofRent)
 }
 
+computeAccumulatedValueOfHome <- function(initialValue, 
+                                          propertyTax, 
+                                          timeHorizon,
+                                          capitalGains,
+                                          propertyAppreciation){
+  
+  futureValueOfHome <- initialValue * (1 + propertyAppreciation - propertyTax)^timeHorizon - 
+    capitalGains * (initialValue * (1 + propertyAppreciation - propertyTax)^timeHorizon - initialValue)
+  
+  return(futureValueOfHome)
+  
+}
 
+
+### Using growth annuity payment formula from http://financeformulas.net/Growing-Annuity-Payment-from-Future-Value.html
 computeAccumulatedValueOfRent <- function(initialAnnualRentPayment,
                                           rentIncreaseRate,
                                           marketReturn,
                                           timeHorizon,
                                           topIncomeTaxRate,
-                                          annualMortgagePayment) {
+                                          annualMortgagePayment, 
+                                          managementFees) {
 
-  accumulatedValueOfRentBeforeMortgagePaidOff <- (initialAnnualRentPayment - annualMortgagePayment) * (1 - topIncomeTaxRate) * (((1 + marketReturn)^timeHorizon -
-    (1 + rentIncreaseRate)^timeHorizon) / (marketReturn - rentIncreaseRate))
+  accumulatedValueOfRentBeforeMortgagePaidOff <- (initialAnnualRentPayment - annualMortgagePayment) * (1 - topIncomeTaxRate) * (1 - managementFees) * 
+    (((1 + marketReturn)^(min(timeHorizon, 30)) -
+    (1 + rentIncreaseRate)^(min(timeHorizon, 30))) / (marketReturn - rentIncreaseRate))
 
   accumulatedValueOfRentAfterMortgagePaidOff <- 0
 
   if(timeHorizon > 30) {
-    annualRentPaymentAfter30Years <-  initialAnnualRentPayment * (rentIncreaseRate)^30
-    accumulatedValueOfRentAfterMortgagePaidOff <- annualRentPaymentAfter30Years * (1 - topIncomeTaxRate) * (((1 + marketReturn)^(timeHorizon - 30) -
+    annualRentPaymentAfter30Years <-  ((1 - topIncomeTaxRate) * (1 - managementFees) * 
+                                         initialAnnualRentPayment * (1 + rentIncreaseRate)^30)
+    
+    accumulatedValueOfRentAfterMortgagePaidOff <- annualRentPaymentAfter30Years * (((1 + marketReturn)^(timeHorizon - 30) -
     (1 + rentIncreaseRate)^(timeHorizon - 30)) / (marketReturn - rentIncreaseRate))
+    
+    accumulatedValueOfRentBeforeMortgagePaidOff <- accumulatedValueOfRentBeforeMortgagePaidOff * (1 + marketReturn)^(timeHorizon - 30)
   }
 
   accumulatedValueOfRent <- accumulatedValueOfRentAfterMortgagePaidOff + accumulatedValueOfRentBeforeMortgagePaidOff
   return(accumulatedValueOfRent)
 }
 
+#Using formula found here: https://www.nerdwallet.com/mortgages/mortgage-calculator/calculate-mortgage-payment
 computeAnnualMortgagePayments <- function (principalRemaining, loanDurationInYears, annualInterestRate) {
     monthlyPayment <- principalRemaining * (annualInterestRate /  12) *
       (((1 + (annualInterestRate / 12)))^(loanDurationInYears * 12)) /
--- tests/testthat/test_helper_functions.R
+++ tests/testthat/test_helper_functions.R
@@ -12,7 +12,7 @@ test_that("computeStockMarketReturns works as expected", {
                                       managementFees = fees
                                       )
 
-  expected <- (1 - taxes) * investment * (1 + return - fees)^time
+  expected <- investment * (1 + return - fees)^time - ((investment * (1 + return - fees)^time) - investment) * taxes
 
   expect_equal(actual, expected)
 })
@@ -27,5 +27,55 @@ test_that("computeAnnualMortgagePayments works as expected", {
                                           annualInterestRate = annualRate)
 
   expected <- 12 * principal * ((annualRate / 12) * (1 + annualRate / 12)^360) / ((1 + (annualRate / 12))^360 - 1)
+  expect_equal(actual, expected)
+})
+
+test_that("computeAccumulatedValueOfRent works as expected for time horizon under 30 years", {
+  initialAnnualRent = 100
+  rentIncrease = .01
+  marketRate = .05
+  time = 20
+  incomeTax = .3
+  annualMortgage = 40
+  fees = .07
+
+  actual <- computeAccumulatedValueOfRent(initialAnnualRentPayment = initialAnnualRent,
+                                          rentIncreaseRate = rentIncrease,
+                                          marketReturn = marketRate, 
+                                          timeHorizon = time, 
+                                          topIncomeTaxRate = incomeTax, 
+                                          annualMortgagePayment = annualMortgage, 
+                                          managementFees = fees)
+  
+  expected <- (initialAnnualRent - annualMortgage) * (1 - incomeTax) * (1 - fees) * 
+    ((1 + marketRate)^time - (1 + rentIncrease)^time) / (marketRate - rentIncrease)
+  expect_equal(actual, expected)
+})
+
+test_that("computeAccumulatedValueOfRent works as expected for time horizon over 30 years", {
+  initialAnnualRent = 100
+  rentIncrease = .01
+  marketRate = .05
+  time = 35
+  incomeTax = .3
+  annualMortgage = 40
+  management = .07
+
+   actual <- computeAccumulatedValueOfRent(initialAnnualRentPayment = initialAnnualRent,
+                                          rentIncreaseRate = rentIncrease,
+                                          marketReturn = marketRate, 
+                                          timeHorizon = time, 
+                                          topIncomeTaxRate = incomeTax, 
+                                          annualMortgagePayment = annualMortgage, 
+                                          managementFees = management)
+  
+  expected <- 
+    
+    (((initialAnnualRent - annualMortgage) * (1 - incomeTax) * (1 - management) *
+    ((1 + marketRate)^30 - (1 + rentIncrease)^30) / (marketRate - rentIncrease))) * (1 + marketRate)^(time - 30) + 
+    
+    
+    (initialAnnualRent * (1 + rentIncrease)^30 * (1 - incomeTax) * (1 - management)) *
+    (((1 + marketRate)^(time - 30) - (1 + rentIncrease)^(time - 30)) / (marketRate - rentIncrease))
   expect_equal(actual, expected)
 })
